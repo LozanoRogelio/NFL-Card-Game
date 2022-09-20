76a09f176df815d4d3af967441a932e6d039ba02
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-09-20 15:52:13 -0500

    Added handle choice function

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+
+
 
   // Shuffle cards Here, random id
   const shuffleCards = () => {
@@ -25,7 +29,10 @@ function App() {
     setTurns(0)
   }
 
-  console.log(cards, turns)
+//  Pick between two cards
+  const handleChoice = (card) => {
+    console.log(card)
+  }
 
 
   return (
@@ -35,7 +42,7 @@ function App() {
 
       <div className='card-grid'>
         {cards.map(card => (
-          <SingleCard key={card.id} card={card}/>
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
         ))}
       </div>
     </div>
