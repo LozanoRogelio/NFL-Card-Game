549445c3aad89d7cf360dee8152f746958a00d1a
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-09-20 13:51:36 -0500

    Created card grid displaying cards in 4 columns. Had to change px of cards to fit screen. Only able to see front and back, not one side at once.

--- src/App.js
+++ src/App.js
@@ -29,8 +29,20 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Magic Match</h1>
+      <h1>Football Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map(card => (
+          <div className='card' key={card.id}>
+            <div>
+              <img className='front' src={card.src} alt='card-front'/>
+              <img className='back'src='/img/card-cover.png' alt='card-back'/>
+
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
