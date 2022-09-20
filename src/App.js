c3938694f52a3692c242a767e39e3b1e07d97d73
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-09-20 13:27:33 -0500

    Created a shuffle cards function that duplicates the array of cards to have 2 of each. Use math.random to set an id in each card and randomize

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { useState } from 'react'
 import './App.css'
 
+const cardImages = [
+  {"src": "/img/chiefs.png"},
+  {"src": "/img/cowboys.png"},
+  {"src": "/img/eagles.png"},
+  {"src": "/img/steelers.png"},
+  {"src": "/img/patriots.png"},
+  {"src": "/img/bills.png"}
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // Shuffle cards Here, random id
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random()- 0.5)
+      .map ((card) => ({...card, id: Math.random()}))
+
+    setCards(shuffleCards)
+    setTurns(0)
+  }
+
+  console.log(cards, turns)
+
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
